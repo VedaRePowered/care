a624070e04aa5863e5890707ce9bbe7f32410d05
Veda <veda@vedapowered.ca>
2024-01-19 13:07:45 -0700

    Elipse tests

--- examples/circles.rs
+++ examples/circles.rs
@@ -1,6 +1,10 @@
 #[care::draw]
 fn draw() {
-    care::graphics::circle((100, 100), 20);
+    care::graphics::circle((100, 100), 30);
+    care::graphics::elipse((400, 100), 20, (2, 0));
+    let s2 = (2.0f32).sqrt();
+    care::graphics::elipse((300.0+s2*50.0, 100.0+s2*50.0), 21, (s2, s2));
+    care::graphics::elipse((300, 200), 20, (0, 2));
     care::graphics::rectangle_rounded((250, 300), (100, 50), 0, [0; 4]);
 }
 
